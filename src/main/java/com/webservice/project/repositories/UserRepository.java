83cde00307a0ac4242b24e0e2606918b9fae0dd9
RuiJ72 <rlagos900@gmail.com>
2023-11-03 22:23:02 -0300

    Refactoring service layer service registration

--- src/main/java/com/webservice/project/repositories/UserRepository.java
+++ src/main/java/com/webservice/project/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.webservice.project.repositories;
 import com.webservice.project.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
