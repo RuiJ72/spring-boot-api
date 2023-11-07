b6c695bedce8c5360ea2ac463712567324a4a41f
RuiJ72 <rlagos900@gmail.com>
2023-11-07 08:40:50 -0300

    Product

---
+++ src/main/java/com/webservice/project/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.webservice.project.repositories;
+
+import com.webservice.project.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
