d20ddbdffcaa8ad15c8ed8a66555ce18df9de66d
RuiJ72 <rlagos900@gmail.com>
2023-11-11 00:00:48 -0300

    Subtotal & Total methods

--- src/main/java/com/webservice/project/entities/Order.java
+++ src/main/java/com/webservice/project/entities/Order.java
@@ -33,6 +33,7 @@ public class Order implements Serializable {
 
     @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
     private Payment payment;
+
     public Order() {
 
     }
@@ -91,6 +92,13 @@ public class Order implements Serializable {
 
     public Set<OrderItem> getItems()  {
         return items;
+    }
+    public Double getTotal() {
+        double sum = 0.0;
+        for (OrderItem x : items) {
+            sum += x.getSubTotal();
+        }
+        return sum;
     }
     @Override
     public boolean equals(Object o) {
