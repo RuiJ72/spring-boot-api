e1514fcd56008e8fddc18321ca7b31326fb507fa
RuiJ72 <rlagos900@gmail.com>
2023-11-20 12:58:02 -0300

    Exception handling - delete

---
+++ src/main/java/com/webservice/project/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.webservice.project.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+}
